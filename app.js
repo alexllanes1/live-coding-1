0f882f6b945f3e5ffde9eff0a21db3f300864d6f
alexllanes1 <alexllanes026@gmail.com>
2023-06-25 22:59:15 -0600

    Ejercicio de live coding

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // Escribe tu código aquí.
+const array = ["Hola", "Como", "AHAHHAHA","Estas", "Me", "Llamo", "Manuel", "Alejandro", "Mucho", "gusto"];
+const parametro = "adios";
+const resultado=[];
+
+const lenght = parametro.length; //5
+const lengthArray = array.length;
+
+for(var i=0;i<lengthArray;i++){
+    if(array[i].length>lenght){
+        resultado.push(array[i]);
+    }
+}
+
+console.log(resultado);
+
+for(var x=0; x<resultado.length;x++){
+let datos1 = document.createElement('li');
+datos1.innerHTML=resultado[x];
+ol.append(datos1);
+}
+
